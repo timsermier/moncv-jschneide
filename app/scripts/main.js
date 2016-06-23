7b184248dbd6fead757918577f3ecb32e609f732
jschneide <jerome.schneide@he-arc.ch>
2016-06-23 15:30:00 +0200

    Ajout de la création de nouveau graphe en doghnuts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,56 @@
+$( document ).ready(function() {
 
-$('a').smoothScroll();
-		
-console.log('\'Allo \'Allo!');
+    $('a').smoothScroll();
+
+    $(".progress").each(function(){
+
+       var size = $(this).find(".progress-bar").attr("aria-valuenow");
+
+       var blank = 100 - size;
+
+       var myCanvas = $("<canvas height='120px'></canvas>");
+
+       var data = {
+          labels: [
+          ],
+          datasets: [
+            {
+                data: [blank, size],
+                backgroundColor: [
+                    "#FFFFFF",
+                    "#36A2EB"
+                    
+                ],
+                borderColor: [
+                    "#FFFFFF",
+                    "#36A2EB"
+                ],
+                hoverBackgroundColor: [
+                    "#FFFFFF",
+                    "#033c73"
+                    
+                ],
+                hoverBorderColor: [
+                    "#FFFFFF",
+                    "#033c73"
+                ]
+            }]
+    };
+
+
+
+       $(this).replaceWith( myCanvas );
+
+       var myDoughnutChart = new Chart(myCanvas, {
+        type: 'doughnut',
+        data: data,
+        options: {
+             tooltips:{enabled:false}
+          }
+        });
+
+    });
+
+
+
+});
